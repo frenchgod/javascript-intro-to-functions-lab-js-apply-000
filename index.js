843b50215c2d027665c413dd4b825f02a57672d8
delightful-certificate-3580 <frenchgod@gmail.com>
2018-10-23 20:56:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
 if (string.toLowerCase() == string){
       return "I can't hear you!" 
 }
-else if (string.toUpperCase()) == string) {
+else if (string.toUpperCase() == string) {
   return "YES INDEED!"
 }
 }
